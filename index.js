eed213783cd1e9034adc741cdb2e64e0e73e85b7
Jennaleigh Angala <jennayey@gmail.com>
2020-06-24 21:58:34 +0800

    Updated layout and styles
    Updated layout
    - layout is now web responsive from web to mobile
    -updated color scheme
    - added functional 'clear' button to clear text

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function translator() {
     let rawText = document.getElementById('rawText').value.toLowerCase();
-    let translatedText = document.getElementById('outputText');
-    translatedText.innerHTML = rawText;
+    let translatedText = document.getElementById('translatedText');
+    translatedText.value = rawText;
+}
+
+function clearText() {
+    let rawText = document.getElementById('rawText');
+    let translatedText = document.getElementById('translatedText');
+
+    rawText.value='';
+    translatedText.value='';
+
 }
